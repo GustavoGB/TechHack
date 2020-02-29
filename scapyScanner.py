5999df96d7060e2916a23e98deadb8b71bb29f44
GustavoGB <gustavo.gobetti98@gmail.com>
2020-02-29 20:28:39 -0300

    ..

--- scapyScanner.py
+++ scapyScanner.py
@@ -7,6 +7,10 @@ import socket
 
 from scapy.all import *
 
+##import ipaddress
+
+import netaddr import * 
+
 if len(sys.argv) != 3:
     print("Uso: %s alvo portaSocket " % (sys.argv[0]))
     print("O range de portas será dado como input do programa")
@@ -15,8 +19,9 @@ if len(sys.argv) != 3:
 alvo = str(sys.argv[1])
 portaSocket = str(sys.argv[2])
 
-ipInicial = inet_ntoa(input("Digite o primeiro IP para comecar a varredura\n"))
-ipFinal   = inet_ntoa(input("Digite o ultimo IP para \n"))
+ipInicial = IPAddress(input("Digite o primeiro IP para comecar a varredura\n"))
+ipFinal   = IPAddress(input("Digite o ultimo IP para \n"))
+
 
 
 portaInicial = int(input("Digite a primeira porta a ser escaneada\n"))
@@ -37,14 +42,18 @@ if portaInicial > portaFinal:
     print("Porta final deve ser menor que porta inicial a ser escaneada")
     sys.exit(0)
 
+
 ## Escanear a rede
 for ips in range(ipInicial,ipFinal):
     ## Escanear as portas
     for p in range(portaInicial,portaFinal):
-        tcp.connect(destino)
-    resposta_pacote = sr1(tcp,timeout=0.5,verbose=0) ## 0x12 significa o sinal syn-ack(funcao sr para send)
-    if resposta_pacote == 0x12: 
-        print('Portas'+str(p)+'estão abertas')
-    sr1(tcp,timeout=0.5,verbose=0)  
+        if tcp.connect_ex((alvo,portaSocket)):
+            print("The port is closed") 
+        else:
+            resposta_pacote = sr1(tcp,timeout=0.5,verbose=0) ## 0x12 significa o sinal syn-ack(funcao sr para send)
+            if resposta_pacote == 0x12: 
+                print('Portas'+str(p)+'estão abertas')
+            sr1(tcp,timeout=0.5,verbose=0)  
 
 print("Escaneamento completo!")
+    
